5680fde70c4b0c0f28d8270550f0e452482e1562
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-06-07 16:55:26 -0700

    Update 221. Maximal Square.cpp

--- 221. Maximal Square.cpp
+++ 221. Maximal Square.cpp
@@ -16,13 +16,15 @@ class Solution {
 public:
     int maximalSquare(vector<vector<char>> &grid) {
         if (grid.empty() || grid[0].empty()) return 0;
-        int m = grid.size(), n = grid[0].size(), res = 0;
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (i == 0 || j == 0) dp[i][j] = grid[i][j] - '0';
-                else if (grid[i][j] == '1') {
-                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i][j - 1], dp[i - 1][j])) + 1;
+        int row = grid.size(), col = grid[0].size();
+        int res = 0;
+        int dp[row][col] = {0};
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (i == 0 || j == 0){
+                    dp[i][j] = grid[i][j] - '0';
+                }else if (grid[i][j] == '1') {
+                    dp[i][j] = min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j])) + 1;
                 }
                 res = max(res, dp[i][j]);
             }
@@ -30,3 +32,9 @@ public:
         return res * res;
     }
 };
+
+
+
+
+
+
