47cf13fefc17a681a2eebdd4a1efa7c60abdad9c
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-06-07 14:12:58 -0700

    Update 206. Reverse Linked List.cpp

--- 206. Reverse Linked List.cpp
+++ 206. Reverse Linked List.cpp
@@ -1,3 +1,14 @@
+/*
+Reverse a singly linked list.
+
+Example:
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+
+Hint:
+A linked list can be reversed either iteratively or recursively. Could you implement both?
+*/
+
 /*Iterative solution: 在原链表之前建立一个dummy node，因为首节点会变，然后从head开始，
 将之后的一个节点移到dummy node之后，重复此操作知道head成为末节点为止*/
 
@@ -9,7 +20,6 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
- 
   
 class Solution {
 public:
@@ -29,6 +39,23 @@ public:
 };
 
 
+/*递归解法的思路是，不断的进入递归函数，直到head指向最后一个节点，p指向之前一个节点，然后调换head和p的位置，
+再返回上一层递归函数，再交换p和head的位置，每次交换后，head节点后面都是交换好的顺序，直到p为首节点，然后再交换，
+首节点就成了为节点，此时整个链表也完成了翻转：
+*/
+
+/* Recursive solution */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode *p = head;
+        head = reverseList(p->next);
+        p->next->next = p;
+        p->next = NULL;
+        return head;
+    }
+};
 
 
 
