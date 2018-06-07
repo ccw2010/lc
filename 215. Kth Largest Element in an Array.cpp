9a85ba0ea05b669fb31cbc17de1dede8855a6828
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-06-07 16:39:26 -0700

    Update 215. Kth Largest Element in an Array.cpp

--- 215. Kth Largest Element in an Array.cpp
+++ 215. Kth Largest Element in an Array.cpp
@@ -49,7 +49,7 @@ class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         int left = 0, right = nums.size() - 1;
-        while (true) {
+        while (left <= right) {
             int pos = partition(nums, left, right);
             if (pos == k - 1) return nums[pos];
             else if (pos > k - 1) right = pos - 1;
@@ -57,17 +57,19 @@ public:
         }
     }
     int partition(vector<int>& nums, int left, int right) {
-        int pivot = nums[left], l = left + 1, r = right;
-        while (l <= r) {
-            if (nums[l] < pivot && nums[r] > pivot) {
-                swap(nums[l++], nums[r--]);
+        int pivot = nums[left], i = left + 1, j = right;
+        while (i <= j) {
+            if (nums[i] < pivot && nums[j] > pivot) {
+                swap(nums[i++], nums[j--]);
             }
-            if (nums[l] >= pivot) ++l;
-            if (nums[r] <= pivot) --r;
+            if (nums[i] >= pivot) i++;
+            if (nums[j] <= pivot) j--;
         }
-        swap(nums[left], nums[r]);
-        return r;
+        swap(nums[left], nums[j]);
+        return j;
     }
 };
+
+
 
 
