0e4facfd3999ceeaf31eaadf0080eefe6349be56
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-06-07 17:19:42 -0700

    Update 23. Merge k Sorted Lists.cpp

--- 23. Merge k Sorted Lists.cpp
+++ 23. Merge k Sorted Lists.cpp
@@ -1,5 +1,4 @@
-23. Merge k Sorted Lists
-DescriptionHintsSubmissionsDiscussSolution
+/*23. Merge k Sorted Lists
 
 Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
 
@@ -11,8 +10,9 @@ Input:
   1->3->4,
   2->6
 ]
-Output: 1->1->2->3->4->4->5->6
 
+Output: 1->1->2->3->4->4->5->6
+*/
 
 /**
  * Definition for singly-linked list.
@@ -22,6 +22,7 @@ Output: 1->1->2->3->4->4->5->6
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
 /*
 The second function is from Merge Two Sorted Lists.
 
@@ -35,25 +36,30 @@ public:
         if(lists.empty()) return nullptr;
     
         while(lists.size() > 1){
-            ListNode* merged = mergeTwoLists(lists[0], lists[1]);
-            lists.push_back(merged);
+            ListNode* mergedList = merge(lists[0], lists[1]);
+            lists.push_back(mergedList);
             lists.erase(lists.begin());
             lists.erase(lists.begin());
         }
         return lists.front();
     }
-    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+    ListNode *merge(ListNode *l1, ListNode *l2) {
         if(!l1) return l2;
         if(!l2) return l1;
         
         if(l1->val <= l2->val){
-            l1->next = mergeTwoLists(l1->next, l2);
+            l1->next = merge(l1->next, l2);
             return l1;
         }
         else{
-            l2->next = mergeTwoLists(l1, l2->next);
+            l2->next = merge(l1, l2->next);
             return l2;
         }
     }
-
 };
+
+
+
+
+
+
