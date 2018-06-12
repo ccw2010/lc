be3d8aef295ddc051f0198cef1454e6cda97b70b
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-06-11 18:37:47 -0700

    Add files via upload

--- 252. Meeting Rooms.cpp
+++ 252. Meeting Rooms.cpp
@@ -19,25 +19,42 @@ Output: true*/
  *     Interval(int s, int e) : start(s), end(e) {}
  * };
  */
-
 class Solution {
 public:
     bool canAttendMeetings(vector<Interval>& intervals) {
         map<int, int> m;
-        for(int i = 0; i < intervals.size(); ++i){
-	    m[intervals[i].start] = intervals[i].end; 
-	}
+        for(auto &x : intervals){
+            m[x.start] = x.end; 
+        }
         /*in case [[8,11],[17,20],[17,20]]*/
-	if(m.size() < intervals.size()) 
-            return false; 
+        if(m.size() < intervals.size()) return false; 
         
-	int lastEnding = 0;
-	for (map<int,int>::iterator it=m.begin(); it!=m.end(); it++){ 
-            if(it->first < lastEnding) 
-                return false;
-            lastEnding = it->second;
+        int lastEnding = 0;
+        for (auto &x : m){ 
+            if(x.first < lastEnding) return false;
+            else lastEnding = x.second;
         }
         return true;
         
     }
 };
+
+/*我们可以先给所有区间排个序，用起始时间的先后来排，然后我们从第二个区间开始，如果开始时间早于前一个区间的结束时间，
+则说明会议时间有冲突，返回false，遍历完成后没有冲突，则返回true*/
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const Interval &a, const Interval &b){return a.start < b.start;});
+        int n = intervals.size();
+        for (int i = 1; i < n; i++) {
+            if (intervals[i].start < intervals[i-1].end) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+
--- 253. Meeting Rooms II.cpp
+++ 253. Meeting Rooms II.cpp
@@ -21,10 +21,37 @@ Output: 1
  * };
  */
 
-/*这道题是之前那道Meeting Rooms的拓展，那道题只让我们是否能参加所有的会，也就是看会议之间有没有时间冲突，
-而这道题让我们求最少需要安排几个会议室，有时间冲突的肯定需要安排在不同的会议室。这道题有好几种解法，先来看
-使用TreeMap来做的，我们遍历时间区间，对于起始时间，映射值自增1，对于结束时间，映射值自减1，然后我们定义结
-果变量res，和房间数rooms，我们遍历TreeMap，时间从小到大，房间数每次加上映射值，然后更新结果res，遇到起始
+
+/* 这道题是252.Meeting Rooms的拓展，252只让我们是否能参加所有的会，也就是看会议之间有没有时间冲突，而这道题让我们求
+最少需要安排几个会议室，有时间冲突的肯定需要安排在不同的会议室。第一种方法是用两个一维数组来做，分别保存起始时间和结束时间，
+然后各自排个序，我们定义结果变量res和结束时间指针endpos，然后我们开始遍历，如果当前起始时间小于结束时间指针的时间，则结果
+自增1，反之结束时间指针自增1，这样我们可以找出重叠的时间段，从而安排新的会议室 */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        vector<int> starts, ends;
+        
+        for (auto &x : intervals) {
+            starts.push_back(x.start);
+            ends.push_back(x.end);
+        }
+        sort(starts.begin(), starts.end());
+        sort(ends.begin(), ends.end());
+
+        int res = 0, endpos = 0;
+        int n = intervals.size();
+        for (int i = 0; i < n; i++) {
+            if (starts[i] < ends[endpos]) res++;
+            else endpos++;
+        }
+        return res;
+    }
+};
+
+
+/*第二种方法是用TreeMap来做的，我们遍历时间区间，对于起始时间，映射值自增1，对于结束时间，映射值自减1，然后我们
+定义结果变量res，和房间数rooms，我们遍历TreeMap，时间从小到大，房间数每次加上映射值，然后更新结果res，遇到起始
 时间，映射是正数，则房间数会增加，如果一个时间是一个会议的结束时间，也是另一个会议的开始时间，则映射值先减
 后加仍为0，并不用分配新的房间，而结束时间的映射值为负数更不会增加房间数*/
 
@@ -32,23 +59,18 @@ class Solution {
 public:
     int minMeetingRooms(vector<Interval>& intervals) {
         map<int, int> m;
-        for (auto x : intervals) {
+        for (auto &x : intervals) {
             m[x.start]++;
             m[x.end]--;
         }
         int rooms = 0, res = 0;
-        for (auto it : m) {
-            res = max(res, rooms += it.second);
+        for (auto &x : m) {
+            rooms += x.second;
+            res = max(res, rooms);
         }
         return res;
     }
 };
-
-
-
-
-
-
 
 
 
