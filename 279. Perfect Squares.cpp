f984198806c89f3cd61b6dabc7fbf80ee8031138
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-06-13 13:49:36 -0700

    Update 279. Perfect Squares.cpp

--- 279. Perfect Squares.cpp
+++ 279. Perfect Squares.cpp
@@ -7,6 +7,9 @@ which sum to n.
 For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 because 13 = 4 + 9.
 */
 
+/*这道题我们可以用动态规划Dynamic Programming来做，我们建立一个长度为n+1的一维dp数组，将第一个值初始化为0，
+其余值都初始化为INT_MAX, i从0循环到n，j从1循环到i+j*j <= n的位置，然后每次更新dp[i+j*j]的值，动态更新dp数组，
+其中dp[i]表示正整数i能少能由多个完全平方数组成，那么我们求n，就是返回dp[n]即可，也就是dp数组的最后一个数字*/
 
 class Solution {
 public:
