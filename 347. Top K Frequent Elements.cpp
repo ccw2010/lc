83f9d9fb07a3df17283bf2fa1f18a6f68dc0e22b
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-06-14 15:15:07 -0700

    Update 347. Top K Frequent Elements.cpp

--- 347. Top K Frequent Elements.cpp
+++ 347. Top K Frequent Elements.cpp
@@ -16,14 +16,14 @@ Your algorithm's time complexity must be better than O(n log n), where n is the 
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> m;
-        priority_queue<pair<int, int>> q;
+        unordered_map<int,int> m;
+        priority_queue<pair<int,int>> q;
         vector<int> res;
         for (auto n : nums)
             m[n]++;
         
-        for (auto it : m) 
-            q.push({it.second, it.first});
+        for (auto &x : m) 
+            q.push({x.second, x.first});
         
         for (int i = 0; i < k; i++) {
             res.push_back(q.top().second); 
