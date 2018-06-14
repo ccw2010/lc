af660850860b68797daff08123dc160cf22c4dc7
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-06-13 20:03:41 -0700

    Update 309.Buy Stock Cooldown.cpp

--- 309.Buy Stock Cooldown.cpp
+++ 309.Buy Stock Cooldown.cpp
@@ -16,6 +16,41 @@ Explanation: transactions = [buy, sell, cooldown, buy, sell]
 
 */
 
+
+/*The series of problems are typical dp. The key for dp is to find the variables to represent the states 
+and deduce the transition function.
+
+Of course one may come up with a O(1) space solution directly, but I think it is better to be generous 
+when you think and be greedy when you implement.
+
+The natural states for this problem is the 3 possible transactions : buy, sell, rest. 
+Here cooldown means no transaction on that day.
+
+Then the transaction sequences can end with any of these three states.
+
+For each of them we make an array, buy[n], sell[n] and cooldown[n].
+
+buy[i]表示在第i天之前最后一个操作是买，此时的最大收益。
+
+sell[i]表示在第i天之前最后一个操作是卖，此时的最大收益。
+
+cooldown[i]表示在第i天之前最后一个操作是冷冻期，此时的最大收益。
+
+我们写出递推式为：
+
+buy[i]  = max(rest[i-1] - price, buy[i-1]) 
+sell[i] = max(buy[i-1] + price, sell[i-1])
+rest[i] = max(sell[i-1], buy[i-1], cooldown[i-1])
+
+上述递推式很好的表示了在买之前有冷冻期，买之前要卖掉之前的股票。一个小技巧是如何保证[buy, rest, buy]的情况不会出现，
+这是由于buy[i] <= rest[i]， 即rest[i] = max(sell[i-1], rest[i-1])，这保证了[buy, rest, buy]不会出现。
+
+另外，由于冷冻期的存在，我们可以得出rest[i] = sell[i-1]，这样，我们可以将上面三个递推式精简到两个：
+
+buy[i]  = max(sell[i-2] - price, buy[i-1]) 
+sell[i] = max(buy[i-1] + price, sell[i-1])
+*/
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
@@ -27,9 +62,18 @@ public:
         buy[0] = -prices[0];
         for(int i=1; i<n; i++){
             buy[i] = max(buy[i-1], cooldown[i-1]-prices[i]);
-            sell[i] = max(sell[i-1], buy[i]+prices[i]);
+            sell[i] = max(sell[i-1], buy[i-1]+prices[i]);
             cooldown[i] = max(cooldown[i-1], sell[i-1]);                       
         }
         return sell[n-1];
     }
 };
+
+
+
+
+
+
+
+
+
