1588155d9e652fa9fc85446b5c687cce34d32a99
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-06-14 15:10:31 -0700

    Update 344. Reverse String.cpp

--- 344. Reverse String.cpp
+++ 344. Reverse String.cpp
@@ -1,25 +1,26 @@
-// Write a function that takes a string as input and returns the string reversed.
+/* 344. Reverse String
+Write a function that takes a string as input and returns the string reversed.
 
-// Example:
-// Given s = "hello", return "olleh".
+Example:
+Given s = "hello", return "olleh". */
+
+/* 直接从两头往中间走，同时交换两边的字符即可
+   Time complexity:  O(n)
+   Space complexity: O(1) */
 
 class Solution {
 public:
-    string reverseString(string s) {
-        // 直接从两头往中间走，同时交换两边的字符即可
-        // Time complexity:  O(n)
-        // Space complexity: O(1)
-        
+    string reverseString(string s) {    
         int left = 0, right = s.size() -1;
         while (left < right){
-            swap(s[left++], s[right--]);
-            // char temp = s[left];
-            // s[left++] = s[right];
-            // s[right--] = temp;
+            swap(s[left], s[right]);
+            left++; right--;
         }
         return s;
     }
 };
+
+
 
 
 
