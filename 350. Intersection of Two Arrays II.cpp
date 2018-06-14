74fe6a159a02b0c1b6c83c110f61ad503d7bbb6e
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-06-14 15:25:03 -0700

    Update 350. Intersection of Two Arrays II.cpp

--- 350. Intersection of Two Arrays II.cpp
+++ 350. Intersection of Two Arrays II.cpp
@@ -15,7 +15,7 @@ What if elements of nums2 are stored on disk, and the memory is limited such tha
 load all elements into the memory at once?
 */
 
-/*这道题是之前那道Intersection of Two Arrays的拓展，不同之处在于这道题允许我们返回重复的数字，
+/*这道题是349.Intersection of Two Arrays的拓展，不同之处在于这道题允许我们返回重复的数字，
 而且是尽可能多的返回，之前那道题是说有重复的数字只返回一个就行。那么这道题我们用哈希表来建立nums1中字符和其出现
 个数之间的映射, 然后遍历nums2数组，如果当前字符在哈希表中的个数大于0，则将此字符加入结果res中，然后哈希表的对应
 值自减1*/
@@ -26,7 +26,9 @@ public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int, int> m;
         vector<int> res;
-        for (auto n : nums1) m[n]++;
+        for (auto n : nums1) {
+            m[n]++;
+        }
         for (auto n : nums2) {
             if (m[n] > 0) res.push_back(n);
             m[n]--;
@@ -34,3 +36,12 @@ public:
         return res;
     }
 };
+
+
+
+
+
+
+
+
+
