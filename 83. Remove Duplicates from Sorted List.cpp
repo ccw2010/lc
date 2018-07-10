6c09a77c0fc9e28a1dee7fe6667695b8b346fb02
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-07-09 20:55:45 -0700

    Add files via upload

--- 83. Remove Duplicates from Sorted List.cpp
+++ 83. Remove Duplicates from Sorted List.cpp
@@ -15,9 +15,9 @@ Given 1->1->2->3->3, return 1->2->3.*/
  
 class Solution {
 public:
-    ListNode* deleteDuplicates(ListNode* head) {
+    ListNode* deleteDuplicates(ListNode *head) {
         if (!head) return NULL;
-        for (ListNode* p = head; p; p = p->next) {
+        for (ListNode *p = head; p; p = p->next) {
             while (p->next && p->val == p->next->val){
                 p->next = p->next->next;
             }
