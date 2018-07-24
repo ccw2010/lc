85be174fef0d1dfd44aaef92d97667e32a499799
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-07-24 11:29:45 -0700

    Update 130. Surrounded Regions.cpp

--- 130. Surrounded Regions.cpp
+++ 130. Surrounded Regions.cpp
@@ -38,39 +38,50 @@ For example:
          X O X X           X 1 X X             X O X X
 */
 
-void solve(vector<vector<char>>& board) {
-    int i,j;
-    int row=board.size();
-    if(!row) return;
-    int col=board[0].size();
+class Solution{
+	
+public:
+	
+  void solve(vector<vector<char>> &board) {
+    int row = board.size();
+    if (!row) return;
+    int col = board[0].size();
 
-	for(i=0;i<row;i++){
-		check(board,i,0,row,col);
-		if(col>1) check(board,i,col-1,row,col);
-	}
-	for(j=1;j+1<col;j++){
-		check(board,0,j,row,col);
-		if(row>1) check(board,row-1,j,row,col);
-	}
-	for(i=0;i<row;i++)
-		for(j=0;j<col;j++)
-			if(board[i][j]=='O')
-				board[i][j]='X';
-	for(i=0;i<row;i++)
-		for(j=0;j<col;j++)
-			if(board[i][j]=='1')
-				board[i][j]='O';
-}
-void check(vector<vector<char> >&m, int i, int j, int row, int col){
-	if(m[i][j]=='O'){
-		m[i][j]='1';
-		if(i>1) check(m,i-1,j,row,col);
-		if(j>1) check(m,i,j-1,row,col);
-		if(i+1<row) check(m,i+1,j,row,col);
-		if(j+1<col) check(m,i,j+1,row,col);
-	}
-}
+    for (int i=0; i < row; i++){
+      check(board, i, 0, row, col);
+      if (col > 1) check(board, i, col-1, row, col);
+    }
 
+    for (int j=1; j+1 < col; j++){
+      check(board, 0, j, row, col);
+      if (row > 1) check(board, row-1, j, row, col);
+    }
+
+    for (int i=0; i < row; i++){
+      for (int j=0; j < col; j++){
+        if (board[i][j]=='O') board[i][j]='X';
+      }
+    }
+
+    for (int i=0; i < row; i++){
+      for (int j=0; j < col; j++){
+        if (board[i][j]=='1') board[i][j] = 'O';
+      }
+    }
+  }
+
+  void check(vector<vector<char>> &board, int i, int j, int row, int col){
+    if (board[i][j] == 'O'){
+      board[i][j] = '1';
+      if (i > 1) check(board, i-1, j, row, col);
+      if (j > 1) check(board, i, j-1, row, col);
+      if (i+1 < row) check(board, i+1, j, row, col);
+      if (j+1 < col) check(board, i, j+1, row, col);
+    }
+  }
+
+
+};
 
 
 
