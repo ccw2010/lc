0d8939892d4afe22fd40815cc13d085abfe519ab
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-07-24 11:30:26 -0700

    Update 130. Surrounded Regions.cpp

--- 130. Surrounded Regions.cpp
+++ 130. Surrounded Regions.cpp
@@ -1,4 +1,5 @@
 /*130. Surrounded Regions
+
 Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
 A region is captured by flipping all 'O's into 'X's in that surrounded region.
 
@@ -51,23 +52,21 @@ public:
       check(board, i, 0, row, col);
       if (col > 1) check(board, i, col-1, row, col);
     }
-
     for (int j=1; j+1 < col; j++){
       check(board, 0, j, row, col);
       if (row > 1) check(board, row-1, j, row, col);
     }
-
     for (int i=0; i < row; i++){
       for (int j=0; j < col; j++){
         if (board[i][j]=='O') board[i][j]='X';
       }
     }
-
     for (int i=0; i < row; i++){
       for (int j=0; j < col; j++){
         if (board[i][j]=='1') board[i][j] = 'O';
       }
     }
+	  
   }
 
   void check(vector<vector<char>> &board, int i, int j, int row, int col){
@@ -80,8 +79,13 @@ public:
     }
   }
 
-
 };
+
+
+
+
+
+
 
 
 
