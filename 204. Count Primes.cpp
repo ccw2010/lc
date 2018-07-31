5607036615d1b0c243651f58844ec304e70e1c76
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-07-31 09:56:48 -0700

    Update 204. Count Primes.cpp

--- 204. Count Primes.cpp
+++ 204. Count Primes.cpp
@@ -8,49 +8,22 @@ Output: 4
 Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 */
 
-class Solution {
-public:
-    int countPrimes(int n) {
-        vector<bool> prime(n - 1, true);
-        prime[0] = false;
-        int res = 0;
-        for (int i = 2; i <= sqrt(n); ++i) {
-            if (prime[i - 1]) {
-                for (int j = i * i; j < n; j += i) {
-                    prime[j - 1] = false;
-                }
-            }
-        }
-        for (int j = 0; j < n - 1; ++j) {
-            if (prime[j]) res++;
-        }
-        return res;
-        
-    }
-};
 
 class Solution {
 public:
     int countPrimes(int n) {
-        vector<bool> prime(n, true);
-        prime[0] = false, prime[1] = false;
+        vector<bool> isPrime(n, true);
+        isPrime[0] = false, isPrime[1] = false;
         for (int i = 0; i < sqrt(n); i++) {
-            if (prime[i]) {
+            if (isPrime[i]) {
                 for (int j = i*i; j < n; j += i) {
-                    prime[j] = false;
+                    isPrime[j] = false;
                 }    
             }    
         }
-        return count(prime.begin(), prime.end(), true);
+        return count(isPrime.begin(), isPrime.end(), true);
     }
 };
-
-
-
-
-
-
-
 
 
 
