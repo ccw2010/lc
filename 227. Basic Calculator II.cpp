a2cfa81e33679260d843feca80edd0cb4a5794c6
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-08-02 09:54:31 -0700

    Update 227. Basic Calculator II.cpp

--- 227. Basic Calculator II.cpp
+++ 227. Basic Calculator II.cpp
@@ -17,40 +17,42 @@ Example 3:
 Input: " 3+5 / 2 "
 Output: 5 */
 
-
 /*这道题是 Basic Calculator 的拓展，不同之处在于那道题的计算符号只有加和减，而这题加上了乘除，那么就牵扯到了运算优先级的问题，
 好在这道题去掉了括号，适当的降低了难度。这道题先按没有括号来处理，由于存在运算优先级，我们采取的措施是使用一个栈保存数字，如果该
 数字之前的符号是加或减，那么把当前数字压入栈中，注意如果是减号，则加入当前数字的相反数，因为减法相当于加上一个相反数。如果之前的
 符号是乘或除，那么从栈顶取出一个数字和当前数字进行乘或除的运算，再把结果压入栈中，那么完成一遍遍历后，所有的乘或除都运算完了，再
 把栈中所有的数字都加起来就是最终结果了。*/
- 
-
 
 class Solution {
 public:
     int calculate(string s) {
-        int res = 0, num = 0, n = s.size();
+        int res = 0, num = 0;
+        int n = s.size();
+        unordered_set<char> ops{'+', '-', '*', '/'};
         char op = '+';
-        stack<int> st;
-        for (int i = 0; i < n; ++i) {
-            if (s[i] >= '0') {
-                num = num * 10 + s[i] - '0';
+        stack<int> t;
+        for (int i = 0; i < n; i++) {
+            char c = s[i];
+            if (isdigit(c)) {
+                num = num * 10 + c - '0';
             }
-            if ((s[i] < '0' && s[i] != ' ') || i == n - 1) {
-                if (op == '+') st.push(num);
-                if (op == '-') st.push(-num);
+            if (ops.count(c) || i == n - 1) {
+                if (op == '+') t.push(num);
+                if (op == '-') t.push(-num);
                 if (op == '*' || op == '/') {
-                    int tmp = (op == '*') ? st.top() * num : st.top() / num;
-                    st.pop();
-                    st.push(tmp);
+                    int tmp;
+                    if (op == '*') tmp = t.top() * num;
+                    if (op == '/') tmp = t.top() / num;
+                    t.pop();
+                    t.push(tmp);
                 }
-                op = s[i];
+                op = c;
                 num = 0;
             } 
         }
-        while (!st.empty()) {
-            res += st.top();
-            st.pop();
+        while (!t.empty()) {
+            res += t.top();
+            t.pop();
         }
         return res;
     }
