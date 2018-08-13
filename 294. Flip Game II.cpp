38763171aa7a039d43c895c6278ffa8c3e6ddccc
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-08-13 11:01:40 -0700

    Update 294. Flip Game II.cpp

--- 294. Flip Game II.cpp
+++ 294. Flip Game II.cpp
@@ -14,20 +14,21 @@ Follow up: Derive your algorithm's runtime complexity. */
 /* 这道题让我们判断先手的玩家是否能赢，那么我们可以穷举所有的情况，用回溯法来解题，也是从第二个字母开始遍历整个字符串，
 如果当前字母和之前那个字母都是+，那么我们递归调用将这两个位置变为--的字符串，如果返回false，说明当前玩家可以赢，结束
 循环返回false */
-
  
 class Solution {
 public:
     bool canWin(string s) {
-        for (int i = 1; i < s.size(); i++) {
-        	bool win = canWin(s.substr(0, i-1) + "--" + s.substr(i+1));
-            if (s[i] == '+' && s[i-1] == '+' && !win) {
-                return true;
+        int n = s.size();
+        for (int i = 0; i < n-1; i++) {
+                if (s.substr(i,2) == "++") {
+                    string flipped = s.substr(0,i) + "--" + s.substr(i+2);
+                    if (!canWin(flipped)) return true;
+                }
             }
-        }
         return false;
     }
 };
+
 
 
 
