d286709ead89512c773de2b17c462a304f747a0f
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-08-29 09:54:49 -0700

    Update 298. Binary Tree Longest Consecutive Sequence.cpp

--- 298. Binary Tree Longest Consecutive Sequence.cpp
+++ 298. Binary Tree Longest Consecutive Sequence.cpp
@@ -44,16 +44,16 @@ public:
     int longestConsecutive(TreeNode* root) {
         if (!root) return 0;
         int res = 0;
-        dfs(root, root->val, 0, res);
+        preorder(root, root->val, 0, res);
         return res;
     }
-    void dfs(TreeNode *root, int v, int count, int &res){
+    void dfs(TreeNode *root, int val, int count, int &res){
         if (!root) return;
-        if (root->val == v+1) count++;
+        if (root->val == val+1) count++;
         else count=1;
         res = max(res, count);
-        dfs(root->left, root->val, count, res);
-        dfs(root->right, root->val, count, res);   
+        preorder(root->left, root->val, count, res);
+        preorder(root->right, root->val, count, res);   
     }
 };
 
