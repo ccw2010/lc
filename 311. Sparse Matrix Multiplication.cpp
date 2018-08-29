b46e5400a2f7de8277a4a124e11ab2d303a772b2
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-08-29 16:44:12 -0700

    Update 311. Sparse Matrix Multiplication.cpp

--- 311. Sparse Matrix Multiplication.cpp
+++ 311. Sparse Matrix Multiplication.cpp
@@ -20,13 +20,18 @@ B = [
 AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
                   | 0 0 1 |
  
-
 这道题让我们实现稀疏矩阵相乘，稀疏矩阵的特点是矩阵中绝大多数的元素为0，而相乘的结果是还应该是稀疏矩阵，即还是大多数元素为0，
 那么我们使用传统的矩阵相乘的算法肯定会处理大量的0乘0的无用功，所以我们需要适当的优化算法，使其可以顺利通过OJ，我们知道一个 
 i x k 的矩阵A乘以一个 k x j 的矩阵B会得到一个 i x j 大小的矩阵C，那么我们来看结果矩阵中的某个元素C[i][j]是怎么来的，
 起始是A[i][0]*B[0][j] + A[i][1]*B[1][j] + ... + A[i][k]*B[k][j]，那么为了不重复计算0乘0，我们首先遍历A数组，要确保
 A[i][k]不为0，才继续计算，然后我们遍历B矩阵的第k行，如果B[k][j]不为0，我们累加结果矩阵res[i][j] += A[i][k] * B[k][j]; 
 这样我们就能高效的算出稀疏矩阵的乘法 */
+
+/* Note:
+The standard multiplication of an m-by-k matrix by an k-by-n matrix has complexity O(mkn). 
+If all of those are "n" to you, it's O(n^3), not O(n^2). 
+EDIT: it will not be O(n^2) in the general case. But there are faster algorithms for particular 
+types of matrices -- if you know more you may be able to do better.*/
 
 
 class Solution {
