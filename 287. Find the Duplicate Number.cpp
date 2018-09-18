645820ecd000c0a1f9b02d9380c2bc5ca3c8a695
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-09-18 16:23:48 -0700

    Update 287. Find the Duplicate Number.cpp

--- 287. Find the Duplicate Number.cpp
+++ 287. Find the Duplicate Number.cpp
@@ -9,6 +9,13 @@ You must not modify the array (assume the array is read only).
 You must use only constant extra space.
 Your runtime complexity should be less than O(n^2).*/
 
+
+/*这道题给了我们n+1个数，所有的数都在[1,n]区域内，首先让我们证明必定会有一个重复数。题目要求我们不能改变原数组，
+不能给原数组排序，又不能用多余空间，那么哈希表也就不用考虑了，又说时间小于O(n2)，也就不能用brute force的方法，
+那也只能考虑用二分搜索法了，我们在区别[1, n]中搜索，首先求出中点mid，然后遍历整个数组，统计所有小于等于mid的数
+的个数，如果个数大于mid，则说明重复值在[mid+1, n]之间，反之，重复值应在[1, mid-1]之间，然后依次类推，直到搜索
+完成，此时的low就是我们要求的重复值 */
+
 class Solution {
 public:
     int findDuplicate(vector<int> &nums) {
@@ -27,10 +34,9 @@ public:
 };
 
 
-
-/* 经过热心网友waruzhi的留言提醒还有一种O(n)的解法, 其核心思想快慢指针在142.Linked List Cycle II中就有应用，
-由于题目限定了区间[1,n]，所以可以巧妙的利用坐标和数值之间相互转换，而由于重复数字的存在，那么一定会形成环，我们用
-快慢指针可以找到环并确定环的起始位置 */
+/* 还有一种O(n)的解法, 其核心思想快慢指针在142.Linked List Cycle II中就有应用，由于题目限定了区间[1,n]，
+所以可以利用坐标和数值之间相互转换，而由于重复数字的存在，那么一定会形成环，我们用快慢指针可以找到环并确定环的
+起始位置 */
 
 class Solution {
 public:
