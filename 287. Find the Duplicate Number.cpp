7343c09878cead775ac11dfdac84339de63ddbae
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-09-18 15:48:31 -0700

    Update 287. Find the Duplicate Number.cpp

--- 287. Find the Duplicate Number.cpp
+++ 287. Find the Duplicate Number.cpp
@@ -11,7 +11,7 @@ Your runtime complexity should be less than O(n^2).*/
 
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
+    int findDuplicate(vector<int> &nums) {
         int low = 1, high = nums.size() - 1;
         while (low < high) {
             int mid = low + (high - low) / 2;
@@ -25,6 +25,34 @@ public:
         return low;
     }
 };
+
+
+
+/* 经过热心网友waruzhi的留言提醒还有一种O(n)的解法, 其核心思想快慢指针在142.Linked List Cycle II中就有应用，
+由于题目限定了区间[1,n]，所以可以巧妙的利用坐标和数值之间相互转换，而由于重复数字的存在，那么一定会形成环，我们用
+快慢指针可以找到环并确定环的起始位置 */
+
+class Solution {
+public:
+    int findDuplicate(vector<int> &nums) {
+        int slow = 0, fast = 0;
+        int t = 0;
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast) break;
+        }
+        while (true) {
+            slow = nums[slow];
+            t = nums[t];
+            if (slow == t) break;
+        }
+        return slow;
+    }
+};
+
+
+
 
 
 
