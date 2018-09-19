011de1bc41dc237f7797d3848f18c5e5ec9e1ac8
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-09-19 16:18:29 -0700

    Update 217. Contains Duplicate.cpp

--- 217. Contains Duplicate.cpp
+++ 217. Contains Duplicate.cpp
@@ -20,7 +20,7 @@ Output: true
 
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
+    bool containsDuplicate(vector<int> &nums) {
         unordered_map<int,int> m;
         for (int n : nums){
             if(m.count(n)) return true;
@@ -34,9 +34,10 @@ public:
 
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
+    bool containsDuplicate(vector<int> &nums) {
         sort(nums.begin(), nums.end());
-        for (int i = 1; i < nums.size(); i++) {
+        int n = nums.size();
+        for (int i = 1; i < n; i++) {
             if (nums[i] == nums[i-1]) return true;
         }
         return false;
